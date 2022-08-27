12a284e89e69c05d8cda93fb4b290b766f9896a0
Steven Kingston <skingston.dev@gmail.com>
2022-08-27 15:54:33 +0100

    Extracts data from code and adds first core tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
-describe('<App />', () => {
-    it('renders without errors', () => {
-        render(<App />)
+describe('App', () => {
+    let appContainer;
+    beforeEach(() => {
+        jest.resetAllMocks();
+        const {container} = render(<App />)
+        appContainer = container
+    })
+    it('has a classname app', () => {
+        expect(appContainer.firstChild.classList.contains('App')).toBe(true)
+
     });
+    it('common elements', () => {
+        screen.getByRole('heading',{level: 2, name: 'Beers'})
+        screen.getByText('Product') 
+        screen.getByText('Temperature')
+        screen.getByText('Status')
+    });
+
+    describe('Table Headings', () => {
+        it('has correct headings', () => { 
+            screen.getByText('Product') 
+            screen.getByText('Temperature')
+            screen.getByText('Status')
+        })
+    })
 });
